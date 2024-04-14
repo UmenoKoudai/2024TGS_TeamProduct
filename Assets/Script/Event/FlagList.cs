c50c84e22034ca96dede258d9faf1bed96f9c7e9
結城 黎 <106291652+YukiR0708@users.noreply.github.com>
2024-04-15 05:02:08 +0900

    [add]イベント・会話システム

---
+++ Assets/Script/Event/FlagList.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// フラグを格納するリスト
+/// </summary>
+/// 参照サイト：https://kurokumasoft.com/2022/04/28/unity-flag-management/#toc1
+
+[CreateAssetMenu(fileName = "New FlagList", menuName = "ScriptableObject/Flags/FlagList")]
+public class FlagList : ScriptableObject
+{
+
+    [SerializeField, Header("フラグを追加するリスト")]
+    List<FlagData> flags = new();
+
+    public List<FlagData> Flags => flags;
+
+    /// <summary> すべてのフラグを初期化(false)する </summary>
+    public void InitFlags()
+    {
+        foreach (FlagData f in flags)
+        {
+            f.InitFlag();
+        }
+    }
+
+    /// <summary> すべてのフラグをTrueにする。※デバッグ用 </summary>
+   public void SetTrueAllFlags()
+    {
+        foreach (FlagData f in flags)
+        {
+            f.SetFlagStatus();
+        }
+    }
+
+
+    /// <summary> 指定したフラグを変更する </summary>
+    public void SetFlag(FlagData flag, bool value = true)
+    {
+        foreach (FlagData f in flags)
+        {
+            if (f == flag)
+            {
+                f.SetFlagStatus(value);
+                Debug.Log($"{f}が{f.IsOn}になった");
+                return;
+            }
+        }
+    }
+
+    /// <summary> 指定したフラグの状態を確認する </summary>
+    public bool GetFlagStatus(FlagData flag)
+    {
+        foreach (FlagData f in flags)
+        {
+            if (f == flag)
+            {
+                return f.IsOn;
+            }
+        }
+
+        return false;
+    }
+
+
+
+}
