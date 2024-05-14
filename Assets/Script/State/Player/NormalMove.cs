cb06362642862a61d53281a8d3c95f74ec502caa
umenokoudai <umenokoudai0805@gmail.com>
2024-05-14 20:54:49 +0900

    [update]1マスずつ移動するように修正

--- Assets/Script/State/Player/NormalMove.cs
+++ Assets/Script/State/Player/NormalMove.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class NormalMove : IStateMachine
 {
-    private Vector2 _direction;
     private CharacterBase _character;
     private bool _isUpMove = true;
     private bool _isLeftMove = false;
     private bool _isRightMove = false;
     private Vector3Int _start;
+    private bool _isMove = false;
+    private Vector3 _direction;
+    private Vector3Int _nextPos;
 
     public NormalMove(CharacterBase character)
     {
         _character = character;
         _start = _character.Map.WorldToCell(_character.transform.position);
-        Debug.Log(_start);
         _character.transform.position = _start;
     }
 
@@ -28,7 +29,15 @@ public class NormalMove : IStateMachine
 
     public void FixedUpdate()
     {
+        if (!_isMove) return;
+        float distance = Vector3.Distance(_nextPos, _character.transform.position);
         _character.Rb.velocity = _direction * _character.Speed;
+        if (distance < 0.5f) 
+        {
+            _isMove = false;
+            _start = _nextPos;
+            _character.Rb.velocity = Vector3.zero;
+        }
     }
 
     public void Update()
@@ -47,10 +56,11 @@ public class NormalMove : IStateMachine
                 _isRightMove = true;
                 _isLeftMove = false;
                 var nextTile = _character.Map.GetCellCenterWorld(new Vector3Int(_start.x + 1, _start.y, _start.y));
-                _start = _character.Map.WorldToCell(nextTile);
-                if (_character.Map.HasTile(_start))
+                _nextPos = _character.Map.WorldToCell(nextTile);
+                if (_character.Map.HasTile(_nextPos))
                 {
-                    _character.transform.Translate(_start);
+                    _direction = _nextPos - _start;
+                    _isMove = true;
                 }
             }
 
@@ -60,10 +70,11 @@ public class NormalMove : IStateMachine
                 _isRightMove = false;
                 _isLeftMove = true;
                 var nextTile = _character.Map.GetCellCenterWorld(new Vector3Int(_start.x - 1, _start.y, _start.y));
-                _start = _character.Map.WorldToCell(nextTile);
+                _nextPos = _character.Map.WorldToCell(nextTile);
                 if (_character.Map.HasTile(_start))
                 {
-                    _character.transform.Translate(_start);
+                    _direction = _nextPos - _start;
+                    _isMove = true;
                 }
             }
         }
@@ -74,11 +85,12 @@ public class NormalMove : IStateMachine
                 _isUpMove = true;
                 _isRightMove = false;
                 _isLeftMove = false;
-                var nextTile = _character.Map.GetCellCenterWorld(new Vector3Int(_start.x, _start.y - 1, _start.y));
-                _start = _character.Map.WorldToCell(nextTile);
+                var nextTile = _character.Map.GetCellCenterWorld(new Vector3Int(_start.x, _start.y + 1, _start.y));
+                _nextPos = _character.Map.WorldToCell(nextTile);
                 if (_character.Map.HasTile(_start))
                 {
-                    _character.transform.Translate(_start);
+                    _direction = _nextPos - _start;
+                    _isMove = true;
                 }
             }
             if (v < 0)
@@ -87,10 +99,11 @@ public class NormalMove : IStateMachine
                 _isRightMove = false;
                 _isLeftMove = false;
                 var nextTile = _character.Map.GetCellCenterWorld(new Vector3Int(_start.x, _start.y - 1, _start.y));
-                _start = _character.Map.WorldToCell(nextTile);
+                _nextPos = _character.Map.WorldToCell(nextTile);
                 if (_character.Map.HasTile(_start))
                 {
-                    _character.transform.Translate(_start);
+                    _direction = _nextPos - _start;
+                    _isMove = true;
                 }
             }
         }
