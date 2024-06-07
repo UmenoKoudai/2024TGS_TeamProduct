f791e8dc7b86657569cdbe0865b4d5ba1e754727
ninomiyahayato <a7pseej9@icloud.com>
2024-06-07 13:34:26 +0900

    [add] ItemのCanvas雛形

--- Assets/Script/Item/ItemPanel.cs
+++ Assets/Script/Item/ItemPanel.cs
@@ -8,6 +8,7 @@ public class ItemPanel : MonoBehaviour
 {
     ItemInventry _itemInventry;
     [SerializeField] Image[] _images;
+    [SerializeField]public Text _itemEffectText;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,29 +16,45 @@ public class ItemPanel : MonoBehaviour
     }
     public void ItemSlot(int count)
     {
-        if (count <= _images.Length)
-        {
-            for (int i = 0; i < _images.Length; i++)
-            {
-                _images[i].sprite = null;
-            }
-            for (int i = 0; i < count; i++)
-            {
-                _images[i].sprite = _itemInventry._itemDataBases[i]._itemSprite;
-            }
-        }
+        //if (count <= _images.Length)
+        //{
+        //    for (int i = 0; i < _images.Length; i++)
+        //    {
+        //        _images[i].sprite = null;
+        //    }
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        _images[i].sprite = _itemInventry._itemDataBases[i]._itemSprite;
+        //    }
+        //}
 
         //ボタンの対応への追記文
 
+        //if (count <= _itemInventry._itemButton.Length)
+        //{
+        //    for (int i = 0; i < _itemInventry._itemButton.Length; i++)
+        //    {
+        //        _itemInventry._itemButton[i].image.sprite = null;
+        //    }
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        _itemInventry._itemButton[i].image.sprite = _itemInventry._itemDataBases[i]._itemSprite;
+        //    }
+        //}
+
+        //ボタン対応への名前変更
         if (count <= _itemInventry._itemButton.Length)
         {
-            for (int i = 0; i < _images.Length; i++)
+            for (int i = 0; i < _itemInventry._itemButton.Length; i++)
             {
                 _itemInventry._itemButton[i].image.sprite = null;
+                Text text = _itemInventry._itemButton[i].GetComponentInChildren<Text>();
+                text.text = null;
             }
             for (int i = 0; i < count; i++)
             {
-                _itemInventry._itemButton[i].image.sprite = _itemInventry._itemDataBases[i]._itemSprite;
+                Text text = _itemInventry._itemButton[i].GetComponentInChildren<Text>();
+                text.text = _itemInventry._itemDataBases[i]._itemName;
             }
         }
     }
